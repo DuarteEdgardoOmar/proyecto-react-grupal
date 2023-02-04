ea7299d65a5dff01e25c7a393be082f2ede4f9e2
Duarte Edgardo Omar <duarteomar1975@gmail.com>
2023-02-04 16:37:03 -0300

    creo carpeta api con db.json

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import React from "react";
+
+import Carousel from "./components/carousel";
+
 
 
 function App() {
   return (
     <div>
       <h1>Hola Mundo!!!</h1>
+
+      <Carousel/>
     </div>
   );
 }
